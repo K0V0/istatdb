33a7e20258a08aca4416522c5fd7b379592c11d6
Matej Kováč <kovo1533@gmail.com>
2017-01-17 15:48:03 +0100

    added google search to goods

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,6 +5,11 @@ var mainHandler = function() {
 
 	ACTIONS_INDEX = new ActionsHandler();
 
+	$(document).on('click', '#search_good_on_google', function() {
+		var q = $('#q_ident_or_description_cont').val();
+		window.open('http://google.com/search?q=' + q);
+	});
+
 	searchQuery(
 		'/api/knnumber_search',
 		{ 
