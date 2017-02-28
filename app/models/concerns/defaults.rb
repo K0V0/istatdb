4c6df69bd7cabb75499ce66f66427f2fd22a36c1
K0V0 <kovo1533@gmail.com>
2017-03-01 00:26:02 +0100

    ordering bug

--- app/models/concerns/defaults.rb
+++ app/models/concerns/defaults.rb
@@ -4,9 +4,9 @@ module Defaults
 	def self.included(base)
 	  base.instance_eval do 
 
-	  	scope :default_order, -> { 
-			order(id: :asc)
-		}
+	  	#scope :default_order, -> { 
+		#	order(id: :asc)
+		#}
 
 	  	scope :order_this_id_first, -> (pars) { 
 			order_as_specified(id: [pars])
