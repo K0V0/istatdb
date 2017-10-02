758b4c33fdfcacb45b00249b2633567b73cbcd6d
K0V0 <kovo1533@gmail.com>
2017-10-03 00:52:14 +0200

    konecne spon cosi ukada sekcia vyrobcou odberatelov 2

--- app/controllers/impexpcompany_manufacturers_controller.rb
+++ app/controllers/impexpcompany_manufacturers_controller.rb
@@ -10,12 +10,31 @@ class ImpexpcompanyManufacturersController < ApplicationController
 	def update_multiple
 		@details = ImpexpcompanyManufacturer.find(params[:impexpcompany_manufacturers].keys)
 		@details.each do |detail|
-			if !(pars = params[:impexpcompany_manufacturers]["#{detail.id}"])[:local_taric_id].blank?
-				# taric code selected from list
+			local_taric_selected = !((pars = params[:impexpcompany_manufacturers]["#{detail.id}"])[:local_taric_id].blank?)
+			adding_new_not_allowed = (pars[:local_taric][:allow_search_as_new] != "1")
+			if local_taric_selected&&adding_new_not_allowed
+				# taric code selected from list and not allowed to use input of searchfield
+				# as data for new LocalTaric object
 				detail.update(permitted_pars(pars.except(:local_taric)))
 			else
 				# new taric code also filled up
-				detail.update(permitted_pars(pars))
+				if !adding_new_not_allowed
+					#pars[:local_taric][:allow_search_as_new] == "1"
+					Rails.logger.info "------------------------"
+					#adding
+					Rails.logger.info pars[:local_taric][:kncode]
+					detail.local_taric = LocalTaric.find_or_create_by(
+						kncode: pars[:local_taric][:kncode],
+						description: pars[:local_taric][:description]
+					)
+
+					Rails.logger.info detail.local_taric
+					Rails.logger.info detail.local_taric.id
+					detail.update_attributes(
+						permitted_pars(pars.except(:local_taric_id, :local_taric))
+					)
+					detail.save
+				end
 			end
 		end
 	end
