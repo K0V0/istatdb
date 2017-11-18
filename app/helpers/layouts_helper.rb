e2f08f832199c590df0e9456200de3a1ba45837c
K0V0 <kovo1533@gmail.com>
2017-11-18 15:25:14 +0100

    started other sections to update to new rendering framework

--- app/helpers/layouts_helper.rb
+++ app/helpers/layouts_helper.rb
@@ -1,6 +1,6 @@
 module LayoutsHelper
 
-	def new_edit(type: nil, obj: nil, assoc: nil, fields: {})
+	def new_edit(type: nil, obj: nil, assoc: nil, fields: {}, multiedit: false)
 		type = :main if type.nil? && assoc.nil?
 		type = :belongs_to if type.nil? && assoc.to_s.is_singular?
 		type = :has_many if type.nil? && !assoc.to_s.is_singular?
@@ -10,8 +10,22 @@ module LayoutsHelper
 			"layouts/shared/new_edit_form/#{type.to_s}",
 			obj: obj,
 			assoc: assoc,
-			fields: fields
+			fields: fields,
+			multiedit: multiedit
 		)
+	end
+
+	def new_edit_multiple(title: nil, form_object: nil, ar_object: nil, result_of_collection: nil, key: :id, name: nil)
+
+		render('layouts/new_checkboxes_table', 
+			title: title,
+			form_object: form_object,
+			ar_object: ar_object,
+			result_of_collection: result_of_collection,
+			key: key,
+			name: name 
+		) 
+
 	end
 
 	def settings(section: :main, settings: {})
