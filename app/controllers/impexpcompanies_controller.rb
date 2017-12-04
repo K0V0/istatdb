85fef75dcc9033b57c9995409860ca7e20916452
K0V0 <kovo1533@gmail.com>
2017-12-04 01:28:37 +0100

    started show sections adding 9

--- app/controllers/impexpcompanies_controller.rb
+++ app/controllers/impexpcompanies_controller.rb
@@ -6,4 +6,17 @@ class ImpexpcompaniesController < ApplicationController
    		params[:impexpcompany].permit(:company_name)
  	end
 
+ 	def show_action
+		super
+		# this include only taric code, incoterm and correct invoices mark
+		# only for impexpcompany shown
+		@for_manufacturerstable = @record.manufacturers
+			.joins(impexpcompany_manufacturers: [:incoterm, :local_taric])
+			.includes(impexpcompany_manufacturers: [:incoterm, :local_taric])
+
+		@for_tarictable = @record.goods
+			.preload(:local_taric)
+			.select('distinct "goods"."local_taric_id"')
+	end
+
 end
