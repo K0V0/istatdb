54626122ca5d0aa389177f229b9691b5b49aae4c
K0V0 <kovo1533@gmail.com>
2018-02-08 01:38:12 +0100

    takmer finally uoms javashitt 4.5

--- app/assets/javascripts/controllersjs/_new_edit/_remove_validation_errors.js
+++ app/assets/javascripts/controllersjs/_new_edit/_remove_validation_errors.js
@@ -6,13 +6,16 @@ RemoveValidationErrors.prototype = {
 	constructor: RemoveValidationErrors,
 
 	init: function() {
-
+		this.onInputRemove();
 	},
 
 	onInputRemove: function() {
-		$(document).find('input.error').on('input', function() {
+		$(document)
+		.find('input.error, textarea.error')
+		.on('input', function() {
 			var klass = $(this).attr('class').replace(/^(error)\s+/, "");
-			
+			$(this).closest('article').find('span.' + klass).remove();
+			$(this).removeClass('error');
 		});
 	}
 
