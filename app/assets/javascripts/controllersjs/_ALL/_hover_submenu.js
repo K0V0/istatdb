7cab67d35a2fd9f93ac0f378fc53304c1ca920a8
K0V0 <kovo1533@gmail.com>
2018-02-26 10:20:58 +0100

    menu show submenu beghavior js 2

--- app/assets/javascripts/controllersjs/_ALL/_hover_submenu.js
+++ app/assets/javascripts/controllersjs/_ALL/_hover_submenu.js
@@ -11,40 +11,42 @@ HoverSubmenu.prototype = {
 		var T = this;
 		// on menu item actions
 		$(document)
-		.find('nav.top_menu > ul.main > div > li')
-		.betterMouseover(400, function() {
-			if ($(this).hasClass('has_submenu')) {
+			.find('nav.top_menu > ul.main > div > li.has_submenu')
+			.betterMouseover(400, function() {
 				T.show_submenu(this);
-			} else {
-				T.hide_submenus();
-			}
-		});
+			});
 		// on submenu actions
 		$(document)
-		.find('nav.top_menu')
-		.on('mouseenter', 'ul.sub', function() {
-			clearTimeout(T.timer);
-		})
-		.on('mouseleave', 'ul.sub', function() {
-			logger("mouselevae happend")
-			T.timer = setTimeout(function() {
-				var active_elem = $(document).find('nav.top_menu').children('ul.main').find('li.active');
-				if (!active_elem.hasClass(T.class_name)) {
-					if (active_elem.hasClass('has_submenu')) {
-						T.show_submenu(active_elem);
-					} else {
-						T.hide_submenus();
+			.find('nav.top_menu')
+			.on('mouseenter', 'ul.sub', function() {
+				clearTimeout(T.timer);
+			})
+			.on('mouseleave', 'ul.sub', function() {
+				T.timer = setTimeout(function() {
+					var active_elem = $(document).find('nav.top_menu').children('ul.main').find('li.active');
+					if (!active_elem.hasClass(T.class_name)) {
+						if (active_elem.hasClass('has_submenu')) {
+							T.show_submenu(active_elem);
+						} else {
+							T.hide_submenus();
+						}
 					}
-				}
-			}, 500)
-		});
+				}, 500)
+			});
+		// hover active fix
+		$(document)
+			.on('mouseenter', 'nav.top_menu > ul.sub.active, nav.top_menu > ul.main > div > li.active', function(e) {
+				$(document).find(e.handleObj.selector).prependClass('hover');
+			})
 	},
 
 	show_submenu: function(ref) {
 		clearTimeout(this.timer);
 		this.class_name = this.getClassName(ref);
 		$(ref).siblings().removeClass('hover');
-		$(ref).prependClass('hover');
+		if (!$(ref).hasClass('active')) {
+			$(ref).prependClass('hover');
+		}
 		var sub_menus = $(document).find('nav.top_menu').children('ul.sub');
 		sub_menus.filter('.'+this.class_name).removeClass('novisible');
 		sub_menus.not('.'+this.class_name).addClass('novisible');
