4f746c928d57fb29f331b5a0872cbb7df4bedda4
K0V0 <kovo1533@gmail.com>
2018-02-27 01:34:45 +0100

    menu show submenu beghavior js final

--- app/assets/javascripts/controllersjs/_ALL/_hover_submenu.js
+++ app/assets/javascripts/controllersjs/_ALL/_hover_submenu.js
@@ -1,7 +1,4 @@
 function HoverSubmenu() {
-	this.class_name;
-	this.timer;
-	this.timer2;
 	this.init();
 }
 
@@ -10,78 +7,69 @@ HoverSubmenu.prototype = {
 
 	init: function() {
 		var T = this;
-		// on menu item actions
-		$(document)
-			.find('nav.top_menu > ul.main > div > li.has_submenu')
-			.betterMouseover(400, function() {
-				if (!$(this).hasClass('active')) {
-					T.show_submenu(this);
-				}
-			});
+		// handle behaviour baased on main menu
 		$(document)
 			.find('nav.top_menu > ul.main > div > li')
-			.not('.has_submenu')
-			.betterMouseover(400, function() {
-				//if (!$(this).hasClass('active')) {
-					T.hide_submenus();
-				//}
+			.betterMouseover(250, function() {
+				if ($(this).hasClass('has_submenu')) {
+					if ($(this).hasClass('active')) {
+						T.hideSubmenus();
+					} else {
+						T.showSubmenu(this);
+					}
+				} else {
+					T.hideSubmenus();
+				}
 			});
-		// on submenu actions
+		// handle leave menu at all
+		$(document).on('mouseleave', 'nav.top_menu', function() {
+			T.hideSubmenus();
+		});
+		// item with active submenu cosmetic fix
+		$(document)
+		.on('mouseenter',
+			'nav.top_menu > ul.main > div > li.active, nav.top_menu > ul.sub',
+			function(e) {
+				$(document).find(e.handleObj.selector).prependClass('hover');
+			}
+		)
+		.on('mouseleave',
+			'nav.top_menu > ul.main > div > li.active, nav.top_menu > ul.sub',
+			function(e) {
+				$(document).find(e.handleObj.selector).removeClass('hover');
+			}
+		);
+	},
+
+	showSubmenu: function(trigger_elem) {
+		var submenus = $(document).find('nav.top_menu').children('ul.sub');
+		var submenu = $(document)
+			.find('nav.top_menu > ul.sub.'+ this.getClassName(trigger_elem));
+
+		submenus.prependClass('novisible');
+		trigger_elem.siblings().removeClass('hover');
+
+		submenu.removeClass('novisible');
+		trigger_elem.prependClass('hover');
+	},
+
+	hideSubmenus: function() {
 		$(document)
 			.find('nav.top_menu')
-			.on('mouseenter', 'ul.sub', function() {
-				clearTimeout(T.timer);
-			})
-			.on('mouseleave', 'ul.sub', function() {
-				T.timer = setTimeout(function() {
-					var active_elem = $(document).find('nav.top_menu').children('ul.main').find('li.active');
-					if (!active_elem.hasClass(T.class_name)) {
-						if (active_elem.hasClass('has_submenu')) {
-							T.show_submenu(active_elem);
-						} else {
-							T.hide_submenus();
-						}
-					}
-				}, 500);
-			});
-		// hover active fix
+			.children('ul.sub')
+			.not('.active')
+			.prependClass('novisible');
 		$(document)
-			.on('mouseenter', 'nav.top_menu > ul.sub.active, nav.top_menu > ul.main > div > li.active', function(e) {
-				$(document).find(e.handleObj.selector).prependClass('hover');
-				clearTimeout(T.timer2);
-			})
-			.on('mouseleave', 'nav.top_menu > ul.sub.active, nav.top_menu > ul.main > div > li.active', function(e) {
-				T.timer2 = setTimeout(function() {
-					$(document).find(e.handleObj.selector).removeClass('hover');
-				}, 500);
-			});
-	},
-
-	show_submenu: function(ref) {
-		clearTimeout(this.timer);
-		this.class_name = this.getClassName(ref);
-		$(ref).siblings().removeClass('hover');
-		if (!$(ref).hasClass('active')) {
-			$(ref).prependClass('hover');
-		}
-		var sub_menus = $(document).find('nav.top_menu').children('ul.sub');
-		sub_menus.filter('.'+this.class_name).removeClass('novisible');
-		sub_menus.not('.'+this.class_name).prependClass('novisible');
-	},
-
-	hide_submenus: function() {
-		var submenu = $(document).find('nav.top_menu').children('ul.sub');
-		submenu.each(function() {
-			if (!$(this).hasClass('active')) {
-				$(this).addClass('novisible');
-				$(document).find('nav.top_menu > ul.main > div > li').removeClass('hover');
-			}
-		})
-
+			.find('nav.top_menu')
+			.children('ul.sub.active')
+			.removeClass('novisible');
+		$(document)
+			.find('nav.top_menu > ul.main > div > li')
+			.removeClass('hover');
 	},
 
 	getClassName: function(ref) {
+		// class name pairing with main menu item name is always on end
 		return $(ref).attr('class').match(/([a-z]+)$/)[0];
 	}
-
 }
