71016eed510f05f7ea9a972aa56c7deb48911780
K0V0 <kovo1533@gmail.com>
2018-03-11 16:25:13 +0100

    dialog

--- app/assets/javascripts/controllersjs/_ALL/_hide_flash_messages.js
+++ app/assets/javascripts/controllersjs/_ALL/_hide_flash_messages.js
@@ -6,13 +6,22 @@ HideFlashMessages.prototype = {
     constructor: HideFlashMessages,
 
     init: function() {
+        var T = this;
         $(document).on('click', 'body > div.flash_messages', function(e) {
             if (e.target == this) {
-                $(this).remove();
+                T.hideMessages();
             }
         });
        $(document).on('click', 'button.cancel_flash', function(e) {
-            $('body > div.flash_messages').remove();
+             T.hideMessages();
        });
+    },
+
+    hideMessages: function() {
+        $('body > div.flash_messages').children().remove();
+        $('body > div.flash_messages').removeClass('visible');
+        /*$('body > div.flash_messages').one('transitionend', function() {
+           $(this).addClass('novisible');
+        });*/
     }
 }
