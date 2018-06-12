60e3f6c05c686a9c2f0edae40197a3f1732a5297
Matej Kováč <kovo1533@gmail.com>
2018-06-12 11:18:15 +0200

    started with global settings framework, textfields optimized and working

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -6,12 +6,30 @@ class SettingsController < ApplicationController
 
 	def index
 		@settings = current_user.settings.l
+		@global_settings = GlobalSettings.get_all
 	end
 
 	def update_settings
+		update_user_settings
+		update_global_settings
+		redirect_to :back
+	end
+
+	private
+
+	def update_user_settings
 		current_user.settings.u(params[:settings])
 		@MEM.settings = current_user.settings.l
-		redirect_to :back
+	end
+
+	def update_global_settings
+		if params.has_key? :global_settings
+			if current_user.is_admin
+				params[:global_settings].each do |key, val|
+					GlobalSettings[key] = val
+				end
+			end
+		end
 	end
 
 end
