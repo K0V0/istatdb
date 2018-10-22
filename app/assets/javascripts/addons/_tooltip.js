e86b2b15630441902e79d047b7e8722cbebab8c7
Matej Kováč <kovo1533@gmail.com>
2018-10-22 21:21:27 +0200

    mechanism of showing tooltip, bug going behind table

--- app/assets/javascripts/addons/_tooltip.js
+++ app/assets/javascripts/addons/_tooltip.js
@@ -8,10 +8,15 @@
 				var ww = $(window).width();
 				var t = $(this).children("sup");
 				var w = t.width();
+				var h = t.height();
 				var o = $(this).offset().left;
+				var o_top = $(this).offset().top;
+				var o_top_table = $(this).closest('table.items, div.content').offset().top;
 				t.css("display", "block");
 				var pos = ((ww-o) > w) ? "left" : "right"
 				t.css(pos, "0");
+				var pos_h = ((o_top-o_top_table) < (h+24)) ? "top" : "bottom";
+				t.css(pos_h, "100%");
 			}
 		});
 	}
