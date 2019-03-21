e4f5df15b4708234b5fb2d9279b601bc9671ed93
K0V0 <kovo1533@gmail.com>
2019-03-21 02:09:00 +0100

    new js handlig uoms - base functioonality

--- app/assets/javascripts/controllersjs/goods/_uom.js
+++ app/assets/javascripts/controllersjs/goods/_uom.js
@@ -14,50 +14,44 @@ Uom.prototype = {
         $('article.impexpcompany_select, article.manufacturer_select')
         .on('change', 'div.tablewrap > table > tbody > tr > td > input', function(e) {
             logger('impexp or manufacturer changed');
-            //logger(e.delegateTarget);
-            //logger(this);
             toto.updateDropdowns(this, e);
         });
 
         // zmena v dropdown menu spravodajskej jednotky alebo dodavatela/odberatela pre danu mernu jednotku
         $(document)
         .on('change', 'article.uoms > div > div > select', function() {
-            if ($(this).attr('id').indexOf(/(_impexpcompany_id|_manufacturer_id)$/) > 0) {
+            if (toto.HELPER.is_dropdown(this)) {
                 logger('dropdown changed');
-            }
-        });
-
-        $(document)
-        .on('click', 'article.uoms > div > div > select', function() {
-            //logger('dropdown clicked');
-            logger($(this).context.id);
-            if ($(this).context.id.search(/(_impexpcompany_id|_manufacturer_id)$/) > 0) {
-                logger('dropdown clicked');
-                toto.HELPER.set_dropdown_state(this, 'clicked');
-                logger($(this));
             }
         });
 
         // ide az po druhom kliknuti - vybrati moznosti
         $(document)
+        .on('click', 'article.uoms > div > div > select', function() {
+            if (toto.HELPER.is_dropdown(this)) {
+                logger('dropdown clicked');
+            }
+        });
+
+        // zaznamena zmenu este po odchode focusu z elementu, napr prepinanie tabom
+        $(document)
         .on('focusout', 'article.uoms > div > div > select', function() {
-            logger('dropdown focused out');
-            //if ($(this).attr('id').indexOf(/(_impexpcompany_id|_manufacturer_id)$/) > 0) {
-                //logger('dropdown focused out');
-            //}
+            if (toto.HELPER.is_dropdown(this)) {
+                logger('dropdown focused out');
+            }
         });
     },
 
     updateDropdowns: function(ref, e) {
+        var toto = this;
         var data = this.HELPER.collect_data_from_item_properties(e.delegateTarget);
         var dropdowns_class = this.HELPER.get_regex_for_dropdowns_class(e.delegateTarget);
 
         $(document).find('article.uoms').find('select').each(function() {
-             //logger($(this));
-             //logger(dropdowns_class);
             if ($(this).attr('id').indexOf(dropdowns_class) > 0) {
                 logger('dropdown found');
-                logger($(this));
+                toto.HELPER.fillup_dropdown_data(this, data);
+                toto.HELPER.after_actions(this, data);
             }
         });
     }
--- app/assets/javascripts/controllersjs/goods/_uom_helper.js
+++ app/assets/javascripts/controllersjs/goods/_uom_helper.js
@@ -11,20 +11,26 @@ UomHelper.prototype = {
     },
 
     collect_data_from_item_properties: function(ref) {
-        var data = [];
+        var data = new UomData();
         $(ref).find('div.tablewrap').find('input:checked').each(function() {
-            var tmp = {
+            data.add({
                 id: $(this).attr('id').replace(/\D+/, ""),
                 name: $(this).closest('td').next().children('label').text()
-            }
-            data.push(tmp);
+            });
         });
-        //logger(data);
         return data;
     },
 
     get_regex_for_dropdowns_class: function(ref) {
         return $(ref).attr('id').replace(/(_select_\d+)$/, "") + "_id";
+    },
+
+    is_dropdown: function(ref) {
+        if ($(ref).context.id.search(/(_impexpcompany_id|_manufacturer_id)$/) > 0) {
+            return true;
+        } else {
+            return false;
+        }
     },
 
     set_dropdown_state: function(ref, state) {
@@ -33,7 +39,34 @@ UomHelper.prototype = {
 
     get_dropdown_state: function(ref) {
         $(ref).data("state");
-    }
+    },
 
+    fillup_dropdown_data: function(ref, data) {
+        $(ref).children('option').each(function() {
+            var id = $(this).val();
+            if ($(this).is(':selected')) {
+                if (data.contains(id)) {
+                    data.remove(id);
+                }
+            } else {
+                if (data.contains(id)) {
+                    data.remove(id);
+                } else {
+                    $(this).remove();
+                }
+            }
+        });
+        $(ref).append(data.generate_options());
+    },
+
+    after_actions: function(ref, data) {
+        if (($(ref).val() == "")&&(data.size() <= 2)) {
+            $(ref).children('option').each(function() {
+                if ($(this).val != "") {
+                    $(this).prop('selected', true);
+                }
+            });
+        }
+    }
 
 }
