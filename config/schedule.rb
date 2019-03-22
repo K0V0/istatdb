eb132648a7eacc60f69eb16dc6fa1845edf15aaf
Matej Kováč <kovo@kovo-notebook>
2019-03-22 16:51:39 +0100

    rake task to download curses error - adding log to investigate

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,5 +20,5 @@
 # Learn more: http://github.com/javan/whenever
 
 every 1.day, at: '4:30 am' do
-  rake 'exchange_rates:load'
+  rake 'exchange_rates:load', :output => {:error => 'error.log', :standard => 'cron.log'}
 end
