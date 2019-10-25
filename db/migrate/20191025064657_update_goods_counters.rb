b9054a7a37f0520fc3f91272bb5be79bf3c5fed4
Matej Kováč <kovo@kovo-notebook>
2019-10-25 09:11:27 +0200

    pridanie couter chace na tovary

---
+++ db/migrate/20191025064657_update_goods_counters.rb
@@ -0,0 +1,6 @@
+class UpdateGoodsCounters < ActiveRecord::Migration
+  def change
+  	 Manufacturer.all.each { |m| Manufacturer.reset_counters(m.id, :goods) }
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191025001438) do
+ActiveRecord::Schema.define(version: 20191025064657) do
 
   create_table "calculators", force: :cascade do |t|
     t.text     "data"
