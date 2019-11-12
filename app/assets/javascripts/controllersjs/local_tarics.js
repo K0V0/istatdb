1e4356f02cd6a4882feedbbf0d542e51268666a2
Matej Kováč <kovo@kovo-notebook>
2019-11-12 19:23:40 +0100

    zabudnuta ciarka

--- app/assets/javascripts/controllersjs/local_tarics.js
+++ app/assets/javascripts/controllersjs/local_tarics.js
@@ -2,7 +2,7 @@ LOCAL_TARICS = {
 
 	_index_search_show_administrative_end_administrative: {
 		search_item_actions: ["once"],
-		trigger_focus_on_searchfield: ['on_reload', 'on_resize']
+		trigger_focus_on_searchfield: ['on_reload', 'on_resize'],
 		local_tarics_validations: ["on_reload"]
 	}
 }
