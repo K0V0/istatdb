d96d7f63a3ffb6f393c1db86dc428b725b89b6fc
Matej Kováč <kovo@kovo-notebook>
2019-11-12 19:19:29 +0100

    uprava js - zistene kedy prestanu ist niektore handlery na strane uzivatela 2

--- app/assets/javascripts/controllersjs/local_tarics.js
+++ app/assets/javascripts/controllersjs/local_tarics.js
@@ -1,8 +1,8 @@
 LOCAL_TARICS = {
 
 	_index_search_show_administrative_end_administrative: {
-		search_item_actions: ["on_ready"],
-		trigger_focus_on_searchfield: ['on_ready', 'on_reload', 'on_change', 'on_resize'],
+		search_item_actions: ["once"],
+		trigger_focus_on_searchfield: ['on_reload', 'on_resize']
 		local_tarics_validations: ["on_reload"]
 	}
 }
