e75cadac03138d1b6142bd59e002b7e667f1c22a
K0V0 <kovo1533@gmail.com>
2019-11-30 05:33:14 +0100

    odstranene nejake navyse subory

--- test/models/good_issue_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GoodIssueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/impexpcompany_issue_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ImpexpcompanyIssueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
