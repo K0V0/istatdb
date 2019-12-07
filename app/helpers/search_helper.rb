8e74411fe137a9b9d70fdceed944e7a92d93370e
K0V0 <kovo1533@gmail.com>
2019-12-07 03:51:33 +0100

    dorobeny js handler na multiple polia - vymazany nejaky nepotreby kod

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -18,7 +18,6 @@ module SearchHelper
 	end
 
 	def render_searchfield(obj: nil, field: nil, pars: nil, index:0)
-
 		output =
 			"<span>
 				#{
@@ -40,58 +39,6 @@ module SearchHelper
 			</span>"
 
 		return output.html_safe
-	end
-
-	def get_selected_manufacturers(obj:nil)
-		if params.deep_has_key?(:q, :manufacturer_filter)
-			if (a = params[:q][:manufacturer_filter]).instance_of?(Array)
-				if a.all? { |x| x.blank? }
-					return nil
-				else
-					return a.map { |x| x.to_i }
-				end
-			else
-				return nil
-			end
- 		end
-	end
-
-	def render_manufacturers_select(obj: nil)
-		manufacturers_selected = get_selected_manufacturers(obj: obj)
- 		output = ""
-
- 		if manufacturers_selected.nil?
- 			@manufacturers.each do |man|
-		 		output += render_manufacturers_select_row(obj:man)
-		 	end
- 		else
-		 	@manufacturers.select { |x| manufacturers_selected.include?(x.id) }.each do |man|
-		 		output += render_manufacturers_select_row(obj:man, checked:true)
-		 	end
-		 	@manufacturers.select { |x| !manufacturers_selected.include?(x.id) }.each do |man|
-		 		output += render_manufacturers_select_row(obj:man)
-		 	end
-		end
-	 	output.html_safe
-	end
-
-	def render_manufacturers_select_row(obj:nil, checked:false)
-		output = "<span>"
-		output += check_box_tag(
-			'q[manufacturer_filter][]',
-			obj.id,
-			checked,
-			{
-				id: "q_manufacturer_filter_#{obj.id}",
-				class: 'skip_events'
-			}
-		)
-		output += ""
-		output += label_tag(
-			"q[manufacturer_filter][#{obj.id}]",
-			obj.name
-		)
-		output += "</span>"
 	end
 
 	def render_multiselect(obj: nil)
