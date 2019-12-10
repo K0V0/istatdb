38f88c6da129f10de0e72a2beec48b9b3bb80a6e
K0V0 <kovo1533@gmail.com>
2019-12-10 01:36:02 +0100

    spravene exporty aj na sekciu odberatelia vyrobcovia

--- app/models/impexpcompany_manufacturer.rb
+++ app/models/impexpcompany_manufacturer.rb
@@ -14,12 +14,54 @@ class ImpexpcompanyManufacturer < ActiveRecord::Base
 
     accepts_nested_attributes_for :local_taric, update_only: true
 
-    def detach_local_taric
-        !self.local_taric_id.nil?
+    after_initialize :def_export_methods
+
+    private
+
+    def export_methods
+        {
+            local_taric: [:kncode, :description],
+            impexpcompany: [:company_name],
+            incoterm: [:shortland],
+            trade_type: [:type]
+        }
     end
 
-    def detach_local_taric= bool
-        update_attribute(:local_taric_id, nil) if (bool == true)||(bool == 'true')
-        # due to patch tor ransack select dropdowns first ID problem
+    def def_export_methods
+        @s = self
+        export_methods.each do |assoc, fields|
+            fields.each do |field|
+                create_instance_method "export_#{assoc.to_s}_#{field.to_s}" do
+                    sanitize_empty(self.try(assoc).try(field))
+                end
+            end
+        end
     end
+
+    def create_instance_method(method_name, &block)
+        if block_given?
+            self.class.send(:define_method, method_name, block)
+        else
+
+        end
+    end
+
+    def sanitize_empty(obj)
+        if obj.blank?
+            return "---"
+        else
+            return obj
+        end
+    end
+
+    def invoices_correct_human
+        if invoices_correct == true
+            I18n.t('is_true')
+        elsif invoices_correct == false
+            I18n.t('is_false')
+        else
+            "---"
+        end
+    end
+
 end
