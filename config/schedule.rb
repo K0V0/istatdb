1ff2a33d02c0590d8cce05e95edffb45a2190d65
Matej Kováč <kovo1533@gmail.com>
2020-01-20 17:53:20 +0100

    sfunkcnene cron tasky zo servera

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,10 +19,20 @@
 
 # Learn more: http://github.com/javan/whenever
 
+set :env_path,    '"$HOME/.rbenv/shims":"$HOME/.rbenv/bin"'
+
+job_type :rake,   %q{ cd :path && PATH=:env_path:"$PATH" RAILS_ENV=:environment bin/rake :task --silent :output }
+job_type :runner, %q{ cd :path && PATH=:env_path:"$PATH" bin/rails runner -e :environment ':task' :output }
+job_type :script, %q{ cd :path && PATH=:env_path:"$PATH" RAILS_ENV=:environment bundle exec bin/:task :output }
+
 set :output, "log/cron_log.log"
 env :PATH, ENV['PATH']
 env :GEM_PATH, ENV['GEM_PATH']
 
 every 1.day, at: '4:30 am' do
+  rake 'exchange_rates:load'
+end
+
+every 1.minute do
   rake 'exchange_rates:load'
 end
--- lib/tasks/exchange_rates.rake
+++ lib/tasks/exchange_rates.rake
@@ -1,6 +1,6 @@
 namespace :exchange_rates do
 
-  desc "TODO"
+  desc "load"
   task load: :environment do
     tempfile = Down.download(GlobalSettings.valuts_adress_history, destination: Rails.root.join('public', 'valuts'))
     File.rename(Dir['public/valuts/down-*.xml'].first, 'public/valuts/valuts-hist.xml') 
